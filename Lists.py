058a1ab464a10f3bd0d57a738bd0db907e86b596
phil-qa <104486392+phil-qa@users.noreply.github.com>
2022-05-26 08:36:31 +0100

    updating the list exercises

--- Lists.py
+++ Lists.py
@@ -1,17 +1,17 @@
 my_list = ["Cherry", "Apple"]
 
-print(my_list)
+print(f"the initial list {my_list}")
 
 #Add an item to the list
 my_list.append("Pear")
 
-print(my_list)
+print(f"adding a pear {my_list}")
 
 #get the first Item
-print(my_list[0])
+print(f"The first item in the list is {my_list[0]}")
 
-#get the last item in the list
-print(my_list[-1])
+#print the last item in the list
+print(f"The last item in the list is {my_list[-1]}")
 
 #remove to assignment from list
 print(f"the current list {my_list}")
@@ -20,11 +20,20 @@ print(f"The new list {my_list}")
 
 additional_list = ["pineapple", "coconut", "lemon"]
 
-#add one list to another
+#add one list to another to the right
 print(f"initial list {my_list}")
 my_list.extend(additional_list)
 print(f"The updated list {my_list}")
 
+#add to the left
+my_list[:0] = ['avocado', 'grapes']
+print(f"added to the left {my_list}")
+
+#insert one somewhere
+my_list.insert(3,"strawberry")
+print(f"Added straberry to position 4 : {my_list}")
+
 #iterate over a list
 for fruit in my_list:
     print(f"a fruit is : {fruit}")
+
