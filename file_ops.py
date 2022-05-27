2aeeb2b02c13ea2ea28dbcdc5842a4b141e9fdae
phil-qa <104486392+phil-qa@users.noreply.github.com>
2022-05-27 08:18:13 +0100

    Adding class serialisation

---
+++ file_ops.py
@@ -0,0 +1,30 @@
+class Client:
+    def __init__(self, line):
+        self.first_name = line[0]
+        self.last_name = line[1]
+        self.city = line[4]
+
+    def __str__(self):
+        return f"The clients name is {self.first_name} {self.last_name} and they live in {self.city}"
+
+
+
+
+
+
+# open a csv
+import csv
+
+clients = []
+
+# read a csv
+with open("uk-500.csv",newline='') as csvfile:
+    read_data = csv.reader(csvfile, delimiter = ',')
+    next(read_data)
+    for row in read_data:
+        # print(row[0])
+        # print(', '.join(row))
+        clients.append(Client(row))
+
+print(clients[0])
+print(clients[1].first_name)
