2f3906c1dc89ab9b312ada9592f112f5daf44dfe
phil-qa <104486392+phil-qa@users.noreply.github.com>
2022-06-06 18:15:15 +0100

    Unit testing green

--- unitTesting.py
+++ unitTesting.py
@@ -1,6 +1,13 @@
 import unittest
 
+class Calculator:
+
+    def add(self, a, b):
+        return (a+b)
+
+
 class TestCalculator(unittest.TestCase):
+    calculator = Calculator()
 
     def test_two_numbers_are_added(self):
         self.assertEqual(self.calculator.add(1,2), 3)
