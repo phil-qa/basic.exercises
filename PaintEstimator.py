6eb4a1481af346312aa52e3e8a2bdaf5e51626fb
phil-qa <104486392+phil-qa@users.noreply.github.com>
2024-01-10 07:43:25 +0000

    adding sysadmin

--- PaintEstimator.py
+++ PaintEstimator.py
@@ -1,44 +1,45 @@
 '''This is a calculator for costing out the painting of rooms
 It uses standard values but is written so that customisation is possible'''
+import math
 
 
-def area_of_wall(wall_length, wall_height):
-    return wall_length * wall_height
+def area_of_surface(length, height):
+    return length * height
 
 
 def cans_needed(area, coverage):
-    return area/coverage
+    return math.ceil(area / coverage)
 
-def cost_of_job(length, height, coverage, cost_per_can):
-    total_cans = cans_needed(area_of_wall(length, height),coverage)
-    total_cost = total_cans*cost_per_can
+
+def cost_of_job(args):
+    total_cans = cans_needed((area_of_surface(args['width'], args['height'])-args['door_area']), args['coverage'])
+    total_cost = total_cans * args['cost']
     return total_cost
 
+
 def door_area(width=1.5, height=2):
-    return width*height
+    return area_of_surface(width, height)
+
 
 def job_parameters():
     width = float(input("wall width "))
     height = float(input("wall height "))
     coverage = float(input("How much does one can cover ?"))
-    cost = float(input("How much does a can cost"))
-    doors = int(input("How many doors"))
-    doors_area = 0
+    cost = float(input("How much does a can cost "))
+    doors = int(input("How many doors "))
+    total_doors_area = 0
     if doors > 0:
-        for door in range(doors):
-            doors_area += doors_area()
+        for door in range(int(doors)):
+            total_doors_area += door_area()
     parameters = {
-        "width" : width,
-        "height" : height,
-        "coverage" : coverage,
-        "cost" : cost,
-        "door_area" :doors_area
+        "width": width,
+        "height": height,
+        "coverage": coverage,
+        "cost": cost,
+        "door_area": total_doors_area
     }
     return parameters
 
-params = job_parameters()
-for k in params:
-    print(f"{k}, {params[k]}")
 
-
-
+def cost_job():
+    return cost_of_job(job_parameters())
